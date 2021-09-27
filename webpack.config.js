80b39359cf1df8e3650f593bf65a318479135afb
Estevan Jantsk <estevan.jantsk@gmail.com>
2021-09-28 01:24:12 +0300

    add .babelrc.json

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,15 @@ const path = require('path');
 
 module.exports = {
   entry: path.resolve(__dirname, './src/index.js'),
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ]
+  },
   output: {
     path: path.resolve(__dirname, './dist'),
     filename: 'bundle.js'
